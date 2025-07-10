e601a9e68d6c1f4ff3d408682f3c3f2c25bb9e76
AndresjbG <andres.gallegos1611@gmail.com>
2025-07-10 10:24:10 -0500

    creacion del componente blog, creacion d ela maqueta, y realizacion del FormControl

--- sistema_blogging_angular/src/app/components/blog/blog.ts
+++ sistema_blogging_angular/src/app/components/blog/blog.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+
+import { Component, NgModule } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, NgForm, NgModel, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-blog',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, ] ,
   templateUrl: './blog.html',
   styleUrl: './blog.css'
 })
 export class Blog {
+
+ reactiveForm: FormGroup;
+
+ constructor(){
+  this.reactiveForm= new FormGroup({
+    titulo:new FormControl('',[]),
+    imagen:new FormControl('',[]), 
+    texto:new FormControl('',[]),
+    fecha:new FormControl('',[])
+  }, []);
+ }
+
+  cargaDatos(){
+    console.log(this.reactiveForm.value);
+
+  }
+  mostrarError=false;
 
 }
